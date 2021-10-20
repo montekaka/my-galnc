3e5f83275e680d7129c95ca923c385e797f076fe
montekaka <josh.chen.jia@gmail.com>
2021-10-19 22:02:39 -0700

    adds routes setting

---
+++ src/screens/DemoPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DemoPage = () => {
+  return (
+    <div>Hello world</div>
+  )
+}
+
+export default DemoPage;
---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+import DemoPage from './DemoPage'
+
+export {
+  DemoPage
+}
