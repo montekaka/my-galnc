6ca50e02d3ec9dd4a7286203fd6b8049508bca86
montekaka <josh.chen.jia@gmail.com>
2021-11-15 13:46:36 -0800

    implements delete profile

--- src/jotais/profileAtom.js
+++ src/jotais/profileAtom.js
@@ -37,45 +37,31 @@ export const deleteProfileAtom = atom(null, (get, set, deleteId) => {
   const currentState = get(profilesAtom);
   const notiData = get(notificationAtom);
 
-  set(profilesAtom,  () => {
-    set(notificationAtom, () => {
-      return {...notiData, 
-        createdTime: new Date(),
-        message: "Deleted profile",
-        status: true,
-      }
-    })
+  set(profilesAtom, async () => {
+    try {
+      const res = await railsApi.delete(`/v1/profiles/${deleteId}`);
+      set(notificationAtom, () => {
+        return {...notiData, 
+          createdTime: new Date(),
+          message: "Deleted profile",
+          status: true,
+        }
+      })
 
-    const _copy = currentState.filter((x, id) => {
-      return x.id !== deleteId
-    }); 
+      const _copy = currentState.filter((x, id) => {
+        return x.id !== deleteId
+      });
 
-    return _copy;
-
-    // try {
-    //   const res = await railsApi.delete(`/v1/profiles/${id}`);
-    //   set(notificationAtom, () => {
-    //     return {...notiData, 
-    //       createdTime: new Date(),
-    //       message: "Deleted profile",
-    //       status: true,
-    //     }
-    //   })
-
-    //   const _copy = currentState.filter((x, id) => {
-    //     return id !== deleteId
-    //   });
-
-    //   return _copy;
-    // } catch {      
-    //   set(notificationAtom, () => {
-    //     return {...notiData, 
-    //       createdTime: new Date(),
-    //       message: "Failed to deleted profile",
-    //       status: true,
-    //     }
-    //   })      
-    //   return currentState;
-    // }
+      return _copy;
+    } catch {      
+      set(notificationAtom, () => {
+        return {...notiData, 
+          createdTime: new Date(),
+          message: "Failed to deleted profile",
+          status: true,
+        }
+      })      
+      return currentState;
+    }
   })
 })
