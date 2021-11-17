ff751528dc629cda11a783a86e691830f7ab1697
montekaka <josh.chen.jia@gmail.com>
2021-11-17 12:06:59 -0800

    adds conditional render

--- src/components/menus/HeaderMenus.js
+++ src/components/menus/HeaderMenus.js
@@ -20,46 +20,52 @@ const HeaderMenus = () => {
     }    
   }
 
-  return (
-    <div>
-      <Nav mode='horizontal' defaultSelectedKeys={['Home']}
-        header={{
-          logo: <img src="//lf1-cdn-tos.bytescm.com/obj/ttfe/ies/semi/webcast_logo.svg" />,
-          text: 'MyGlance'
-        }}      
-      >
-        <Link to="/" style={{ textDecoration: 'none' }}><Nav.Item itemKey='home' text='Home' icon={<IconHome size="large" />} /></Link>
-        <Link to="/new-profile" style={{ textDecoration: 'none' }}><Nav.Item itemKey='newprofile' text='New Profile' icon={<IconUserAdd size="large" />} /></Link>
-        <Nav.Footer>  
-        <Button
-            theme="borderless"
-            icon = {<IconHelpCircle size="large"/>}
-            style={{
-              color:'var(--semi-color-text-2)',
-              marginRight: '12px',
-            }}
-            onClick={switchMode}
-          />             
-          <Dropdown
-            trigger={'click'}
-            showTick
-            position={'bottomLeft'}
-            clickToHide={true}
-            render={
-              <Dropdown.Menu>
-                  <Dropdown.Title>{user.email}</Dropdown.Title>                
-                  <Dropdown.Item type="secondary" onClick={() => {
-                    signOutUser()
-                  }}>Sign out</Dropdown.Item>
-              </Dropdown.Menu>
-            }
-          >                 
-            <Avatar color='orange' size='small'>{user.email[0].toUpperCase()}</Avatar>
-          </Dropdown>          
-        </Nav.Footer>
-      </Nav>
-    </div>
-  )
+  if(user && user.uid) {
+    return (
+      <div>
+        <Nav mode='horizontal' defaultSelectedKeys={['Home']}
+          header={{
+            logo: <img src="//lf1-cdn-tos.bytescm.com/obj/ttfe/ies/semi/webcast_logo.svg" />,
+            text: 'MyGlance'
+          }}      
+        >
+          <Link to="/" style={{ textDecoration: 'none' }}><Nav.Item itemKey='home' text='Home' icon={<IconHome size="large" />} /></Link>
+          <Link to="/new-profile" style={{ textDecoration: 'none' }}><Nav.Item itemKey='newprofile' text='New Profile' icon={<IconUserAdd size="large" />} /></Link>
+          <Nav.Footer>  
+          <Button
+              theme="borderless"
+              icon = {<IconHelpCircle size="large"/>}
+              style={{
+                color:'var(--semi-color-text-2)',
+                marginRight: '12px',
+              }}
+              onClick={switchMode}
+            />             
+            <Dropdown
+              trigger={'click'}
+              showTick
+              position={'bottomLeft'}
+              clickToHide={true}
+              render={
+                <Dropdown.Menu>
+                    <Dropdown.Title>{user.email}</Dropdown.Title>                
+                    <Dropdown.Item type="secondary" onClick={() => {
+                      signOutUser()
+                    }}>Sign out</Dropdown.Item>
+                </Dropdown.Menu>
+              }
+            >                 
+              <Avatar color='orange' size='small'>{user.email[0].toUpperCase()}</Avatar>
+            </Dropdown>          
+          </Nav.Footer>
+        </Nav>
+      </div>
+    )
+
+    return null;
+  } 
+
+  
 }
 
 export default HeaderMenus;
