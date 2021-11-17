d3d41ad6d00080c2a1b83ae1d2a6c80a06ca0d8a
montekaka <josh.chen.jia@gmail.com>
2021-11-17 11:43:55 -0800

    udpates the component package

--- src/components/profiles/SocialNetworks.js
+++ src/components/profiles/SocialNetworks.js
@@ -2,9 +2,20 @@ import React from "react";
 import { Form, Button, Col, Row} from '@douyinfe/semi-ui';
 import FormInput from './FormInput'
 
-const options = [
+const options = [  
   {icon_name: 'twitter', name: "Twitter", label: 'Twitter', value: 'twitter'}, 
-  {icon_name: 'medium', name: "Medium", label: 'Medium', value: 'medium'}
+  {icon_name: 'linkedin', name: "Linkedin", label: 'Linkedin', value: 'linkedin'},
+  {icon_name: 'github', name: "Github", label: 'Github', value: 'github'},
+  {icon_name: 'medium', name: "Medium", label: 'Medium', value: 'medium'},
+  {icon_name: 'hashnode', name: "Hashnode", label: 'Hashnode', value: 'hashnode'},
+  {icon_name: 'dev', name: "Dev", label: 'Dev', value: 'dev'},
+  {icon_name: 'behance', name: "Behance", label: 'Behance', value: 'behance'},
+  {icon_name: 'dribbble', name: "Dribbble", label: 'Dribbble', value: 'dribbble'},
+  {icon_name: 'facebook', name: "Facebook", label: 'Facebook', value: 'facebook'},
+  {icon_name: 'linkedin', name: "LinkedIn", label: 'LinkedIn', value: 'linkedin'},
+  {icon_name: 'product-hunt', name: "ProductHunt", label: 'ProductHunt', value: 'product-hunt'},
+  {icon_name: 'youtube', name: "Youtube", label: 'Youtube', value: 'youtube'},
+  {icon_name: 'website', name: "Website", label: 'Website', value: 'website'},
 ]
 
 const SocialNetworks = (props) => {
